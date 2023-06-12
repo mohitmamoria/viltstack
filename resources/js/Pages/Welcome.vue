9ecb47381e3ea23466b665c625e548d280b2b395
Mohit Mamoria <mohit.mamoria@gmail.com>
2023-06-12 19:04:40 +0530

    Installed inertia

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { Head } from "@inertiajs/vue3";
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <div
+        class="bg-dots-darker dark:bg-dots-lighter relative min-h-screen bg-gray-100 bg-center selection:bg-red-500 selection:text-white dark:bg-gray-900 sm:flex sm:items-center sm:justify-center"
+    >
+        <h1 class="text-3xl font-bold text-white">Make something useful.</h1>
+    </div>
+</template>
