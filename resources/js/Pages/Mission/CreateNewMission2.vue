31f3105934cdb0ae00e94331deec0a8a735e6bdd
Mohit Mamoria <mohit.mamoria@gmail.com>
2023-06-27 16:33:30 +0530

    cleaned up the code

--- resources/js/Pages/Mission/CreateNewMission2.vue
+++ resources/js/Pages/Mission/CreateNewMission2.vue
@@ -6,94 +6,42 @@
     import Modal from '@/Components/Modal.vue';
     import TextInput from '@/Components/TextInput.vue';
     import { useAPIForm } from '@/Composables/useAPIForm';
-    import { useForm } from '@inertiajs/vue3';
+    import Overlayer from '@/overlayer';
+    import CreateNewMission from './CreateNewMission.vue';
 
-    const form = useForm({
+    const form = useAPIForm({
         url: '',
     });
-
-    const apiForm = useAPIForm({
-        url: '',
-        foo: 'bar',
-    });
-    console.log(apiForm);
 
     const submit = (resolve) => {
-        form.clearErrors();
-        form.processing = true;
-        axios
-            .post(route('api.missions.store'), form.data())
-            .then((response) => {
-                console.log(response);
-                resolve(response.data);
-            })
-            .catch((error) => {
-                if (error.response.status === 422) {
-                    Object.keys(error.response.data.errors).forEach((key) => {
-                        form.setError(key, error.response.data.errors[key][0]);
-                    });
-                    console.log(error.response.data.errors);
-                }
-            })
-            .finally(() => {
-                form.processing = false;
-            });
-        // form.post(route('missions.store'), {
-        //     preserveScroll: true,
-        //     preserveState: true,
-        //     onSuccess: () => resolve({ success: true }),
-        // });
-    };
+        Overlayer.show(CreateNewMission);
 
-    const submit2 = (resolve) => {
-        console.log('submitting 2');
-        apiForm.post(route('api.missions.store'), {
-            onBefore: () => {
-                console.log('before');
-            },
-            onStart: () => {
-                console.log('start');
-            },
+        form.post(route('api.missions.store'), {
             onSuccess: (response) => {
-                console.log('yay!', response);
-            },
-            onError: (error) => {
-                console.log('oops!', error);
-            },
-            onProgress: (event) => {
-                console.log('progress!', event);
-            },
-            onFinish: () => {
-                console.log('finished!');
+                resolve(response);
             },
         });
-        // .then((response) => {
-        //     console.log(response);
-        // })
-        // .catch((errors) => {
-        //     console.log(errors);
-        // });
     };
 </script>
 
 <template>
     <Modal title="New Mission" description="Start with a URL and then add rewards for the winners">
         <template #default="{ resolve }">
-            <form @submit.prevent="submit2(resolve)">
+            <form @submit.prevent="submit(resolve)">
                 <div class="space-y-6">
                     <div>
                         <InputLabel>URL</InputLabel>
-                        <TextInput v-model="apiForm.url" id="url" type="url" class="mt-2 block w-full" required />
-                        <InputError :message="apiForm.errors.url"></InputError>
+                        <TextInput v-model="form.url" id="url" type="url" class="mt-2 block w-full" required />
+                        <InputError :message="form.errors.url"></InputError>
                     </div>
-                    <Card> {{ apiForm }} </Card>
+                    <Card> {{ form.errors }} </Card>
                 </div>
             </form>
         </template>
 
         <template #footer="{ resolve, reject }">
             <Button hierarchy="secondary" type="button" @click="reject()"> Cancel </Button>
-            <Button @click.prevent="submit2(resolve)" :class="{ 'opacity-25': form.processing }">Create</Button>
+            <Button @click.prevent="submit(resolve)" :class="{ 'opacity-25': form.processing }">Create</Button>
         </template>
     </Modal>
 </template>
